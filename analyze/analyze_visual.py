84fce2dfd683f9f04c5d84b1b484779d4effa040
onikazu <programingmanagement@gmail.com>
2018-11-20 15:51:59 +0900

    fix

--- analyze/analyze_visual.py
+++ analyze/analyze_visual.py
@@ -1,14 +1,14 @@
-import robo_tools
+import analyze.robo_tools
 
 def analyzeVisualMessage(message, play_mode):
     OUT_OF_RANGE = 999
-    time = int(robo_tools.getParam(message, "see", 1))
+    time = int(analyze.robo_tools.getParam(message, "see", 1))
     if time < 1:
         return
-    d_neck = robo_tools.getNeckDir(message)
+    d_neck = analyze.robo_tools.getNeckDir(message)
     if d_neck == OUT_OF_RANGE:
         return
-    if robo_tools.checkInitialMode(play_mode):
+    if analyze.robo_tools.checkInitialMode(play_mode):
         d_x = self.m_dKickOffX
         d_y = self.m_dKickOffY
 
@@ -17,9 +17,9 @@ def analyzeVisualMessage(message, play_mode):
     d_y = pos["y"]
     if message.find("(b)") == -1:
         return
-    ball_dist = robo_tools.getParam(message, "(b)", 1)
-    ball_dir = robo_tools.getParam(message, "(b)", 2)
-    rad = math.radians(robo_tools.normalizeAngle(d_neck + ball_dir))
+    ball_dist = analyze.robo_tools.getParam(message, "(b)", 1)
+    ball_dir = analyze.robo_tools.getParam(message, "(b)", 2)
+    rad = math.radians(analyze.robo_tools.normalizeAngle(d_neck + ball_dir))
     d_ball_x = d_x + ball_dist * math.cos(rad)
     d_ball_y = d_y + ball_dist * math.sin(rad)
 
